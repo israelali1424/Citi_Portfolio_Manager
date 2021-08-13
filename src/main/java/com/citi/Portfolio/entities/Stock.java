e546dee898426e0013d76e83593b99e28a45b65b
unknown <israelali1424@gmail.com>
2021-08-13 15:41:56 +0000

    made Stock entity

--- src/main/java/com/citi/Portfolio/entities/Stock.java
+++ src/main/java/com/citi/Portfolio/entities/Stock.java
@@ -1,4 +1,52 @@
 package com.citi.Portfolio.entities;
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+@Table(name="stocks")
 public class Stock {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="symbol")
+    private String symbol;
+
+    @Column(name="volume")
+    private Integer volume;
+    @Column(name="price");
+    private Double price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
